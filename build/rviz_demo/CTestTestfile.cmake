fc3c602db19ecc380adf9cb9e1a2a1c006358178
home <cjkreienkamp@gmail.com>
2024-09-06 09:34:02 +0200

    changing root git folder from catkin workspace to catkin package

--- build/rviz_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/home/code/rviz_demo_ws/src/rviz_demo
-# Build directory: /home/home/code/rviz_demo_ws/build/rviz_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
